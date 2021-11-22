62e3f77071b76216d6dd6a3ac35363c1676f1136
flamingmelon <oscarmcginty@gmail.com>
2021-11-22 12:29:16 +0100

    Added a bag of letters

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-'''
+"""
 Plan:
 - Simulate the scrabble game. Each turn has four states:
     - Board
@@ -23,4 +18,45 @@ Plan:
     Then look at improvements:
     - Make long-term (not greedy) decisions.
     - Try adding letters onto the end of words to make a new slot
-'''
+"""
+
+import string
+
+
+class LettersBag:
+    """Initialize the full bag of letters."""
+
+    def __init__(self):
+        self.all_letters = list(string.ascii_uppercase) + ['blank']
+        self.letter_points = {0: ['blank'],
+                              1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
+                              2: ['D', 'G'],
+                              3: ['B', 'C', 'M', 'P'],
+                              4: ['F', 'H', 'V', 'W', 'Y'],
+                              5: ['K'],
+                              8: ['J', 'X'],
+                              10: ['Q', 'Z']
+                              }
+        self.letter_frequencies = {1: ['J', 'K', 'Q', 'X', 'Z'],
+                                   2: ['B', 'C', 'F', 'H', 'M', 'P', 'V', 'W', 'Y', 'blank'],
+                                   3: ['G'],
+                                   4: ['D', 'L', 'S', 'U'],
+                                   6: ['N', 'R', 'T'],
+                                   8: ['O'],
+                                   9: ['A', 'I'],
+                                   12: ['E']
+                                   }
+
+    def print_all_letters(self):
+        all_letters = []
+        for freq, letters in self.letter_frequencies.items():
+            for letter in letters:
+                for i in range(freq):
+                    all_letters.append(letter)
+        print(all_letters)
+
+
+if __name__ == '__main__':
+
+    new_bag = LettersBag()
+    new_bag.print_all_letters()
